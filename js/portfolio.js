d5286b5eb44cefacb21b60bd30083c3a2e46e494
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2021-08-26 15:42:02 +0530

    motion gifs replaced with mp4s

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,9 +13,13 @@ xhr.send()
 var postSnippet = post => {
     var string = ''
     string += `<div class="post">
-    <div class="post_image_flex">
-        <img src="https://ik.imagekit.io/gavin/gavinpereira/tr:w-450/${post.image}" class="post_image">
-    </div>
+    <div class="post_image_flex">`
+    if (post.image)
+        string += `<img src="https://ik.imagekit.io/gavin/gavinpereira/tr:w-450/${post.image}" class="post_image">`
+    if (post.video)
+        string += `<video src="https://ik.imagekit.io/gavin/gavinpereira/tr:w-450/${post.video}" class="post_image" loop autoplay muted></video>`
+
+    string += `</div>
     <h4 class="post_title">${post.name}</h4>
     <div class="post_tags">`
 
