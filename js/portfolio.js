f52296d1df4ee0e025b2b912d19fff68c5986b73
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2021-09-03 22:09:01 +0530

    lightbox added to home

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,9 +8,6 @@ xhr.open("GET", url, true)
 xhr.send()
 
 
-var lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.append(lightbox)
 
 
 var postSnippet = post => {
@@ -18,7 +15,7 @@ var postSnippet = post => {
     string += `<div class="post">
     <div class="post_image_flex">`
     if (post.image)
-        string += `<img src="https://ik.imagekit.io/gavin/gavinpereira/tr:w-450,pr-true/${post.image}" class="post_image" data-slug="${post.image}">`
+        string += `<img src="https://ik.imagekit.io/gavin/gavinpereira/tr:w-450,pr-true,q-70/${post.image}" loading="lazy" class="post_image" data-slug="${post.image}">`
     if (post.video)
         string += `<video src="https://ik.imagekit.io/gavin/gavinpereira/tr:w-450/${post.video}" class="post_image" loop autoplay muted data-slug="${post.video}"></video>`
 
@@ -79,46 +76,5 @@ var injectAll = posts => {
 
 
 
-
-var lightboxListener = () => {
-    var images = $$('.post_image')
-    images.forEach(image => {
-        image.addEventListener('click', e => {
-            lightbox.classList.add('active')
-            var slug = image.dataset.slug
-            var videoTag = $('video', e.target.parentElement.parentElement)
-            var imgTag = $('img', e.target.parentElement.parentElement)
-
-
-            if (imgTag) {
-                var img = document.createElement('img')
-                img.src = image.src
-                img.srcset = ` https://ik.imagekit.io/gavin/gavinpereira/tr:w-450,pr-true/${slug} 500w,
-                https://ik.imagekit.io/gavin/gavinpereira/tr:w-500,pr-true/${slug} 700w,
-                https://ik.imagekit.io/gavin/gavinpereira/tr:w-800,pr-true/${slug} 1000w,
-                `
-                lightbox.innerHTML = ''
-                lightbox.appendChild(img)
-            }
-            if (videoTag) {
-                var img = document.createElement('video')
-                img.setAttribute('controls', 'true')
-                img.src = `https://ik.imagekit.io/gavin/gavinpereira/tr:w-800/${slug}`
-                // img.srcset = ` https://ik.imagekit.io/gavin/gavinpereira/tr:w-450,pr-true/${slug} 500w,
-                //             https://ik.imagekit.io/gavin/gavinpereira/tr:w-500,pr-true/${slug} 700w,
-                //             https://ik.imagekit.io/gavin/gavinpereira/tr:w-800,pr-true/${slug} 1000w,
-                //             `
-                lightbox.innerHTML = ''
-                lightbox.appendChild(img)
-            }
-
-        })
-    })
-}
-
-lightbox.addEventListener('click', e => {
-    if (e.target !== e.currentTarget) return
-    lightbox.classList.remove('active')
-})
 
 
