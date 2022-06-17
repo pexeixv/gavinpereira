09db4e0267fce25faec9d82a56a56969564d2e53
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2022-06-17 11:38:08 +0530

    * redirect issue fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,26 @@
-const dateFilter = date => {
-    const dateArray = date.toString().split(' ')
-    return `${dateArray[2]} ${dateArray[1]} ${dateArray[3]}`
-}
+const dateFilter = (date) => {
+  const dateArray = date.toString().split(" ");
+  return `${dateArray[2]} ${dateArray[1]} ${dateArray[3]}`;
+};
 
-module.exports = eleventyConfig => {
-    eleventyConfig.addPassthroughCopy('./sass/styles.css')
-    eleventyConfig.addPassthroughCopy('./img')
-    eleventyConfig.addPassthroughCopy('./js')
-    eleventyConfig.addPassthroughCopy('./font')
-    eleventyConfig.addPassthroughCopy('./_redirects')
-    eleventyConfig.addPassthroughCopy('./_headers')
-    eleventyConfig.addPassthroughCopy('./_data/posts.json')
-    eleventyConfig.addPassthroughCopy('./sitemap.xml')
-    eleventyConfig.addFilter('lowerAndHyphen', string => string.toLowerCase().replace(/\s/g, "-"))
-    eleventyConfig.addFilter('dateFilter', dateFilter)
-    return {
-        dir: {
-            input: '.',
-            output: '_site'
-        }
-    }
-}
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy("./sass/styles.css");
+  eleventyConfig.addPassthroughCopy("./img");
+  eleventyConfig.addPassthroughCopy("./js");
+  eleventyConfig.addPassthroughCopy("./font");
+  eleventyConfig.addPassthroughCopy("./_redirects");
+  eleventyConfig.addPassthroughCopy("./_headers");
+  eleventyConfig.addPassthroughCopy("./_data/posts.json");
+  eleventyConfig.addPassthroughCopy("./sitemap.xml");
+  eleventyConfig.addPassthroughCopy("./robots.txt");
+  eleventyConfig.addFilter("lowerAndHyphen", (string) =>
+    string.toLowerCase().replace(/\s/g, "-")
+  );
+  eleventyConfig.addFilter("dateFilter", dateFilter);
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+    },
+  };
+};
