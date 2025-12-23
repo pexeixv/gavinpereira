1b22850ff32b3b1c0d138a30dbdfe1bdd08eda91
Gavin Pereira <gavin.pereira@envistaco.com>
2025-12-24 01:14:38 +0530

    Restructure project folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,18 @@
 module.exports = (eleventyConfig) => {
-    eleventyConfig.addPassthroughCopy("./sass/styles.css")
-    eleventyConfig.addPassthroughCopy("./img")
-    eleventyConfig.addPassthroughCopy("./js")
-    eleventyConfig.addPassthroughCopy("./font")
-    eleventyConfig.addPassthroughCopy("./_data/posts.json")
-    eleventyConfig.addPassthroughCopy({ "./public/*": "/" })
-    eleventyConfig.addFilter("lowerAndHyphen", string => string.toLowerCase().replace(/\s/g, "-"))
-    return {
-        dir: {
-            input: ".",
-            output: "_site"
-        }
-    }
-}
+  eleventyConfig.addPassthroughCopy("src/sass/styles.css");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/font");
+  eleventyConfig.addPassthroughCopy("src/_data/posts.json");
+  eleventyConfig.addPassthroughCopy({ "src/public/*": "/" });
+  eleventyConfig.addFilter("lowerAndHyphen", (string) =>
+    string.toLowerCase().replace(/\s/g, "-")
+  );
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+    templateFormats: ["njk", "html"],
+  };
+};
